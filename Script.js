7cd565cf70245bc98fcad6ca437874588e29d8eb
Gabriela-Serafin <w72395@student.wsiz.edu.pl>
2025-07-04 10:08:29 +0200

    Final changes

--- Script.js
+++ Script.js
@@ -6,14 +6,16 @@ function validateForm(event){
     const name = document.getElementById('name').value.trim();
     const email = document.getElementById('email').value.trim();
     let age = document.getElementById('age').value.trim();
-    const message= document.getElementById('message');
+    const hobby = document.getElementById('hobby').value.trim();
+    const message = document.getElementById('message');
+ 
 
     message.textContent = "";
     message.style.color = "red";
 
 
     if (name === "" || email === "" || age === "") {
-       message.textContent = " All information is required";
+       message.textContent = " All information are required";
        return "";
     }
 
@@ -33,23 +35,24 @@ function validateForm(event){
         return;
     }
 
-    const  person = {name, email, age};
+    const  person = {name, email, age, hobby};
     addPersonToList(person);
 
 
     message.style.color = "green";
-    message.textConent = "Submitted";
+    message.textContent = "Submitted";
 
     document.getElementById('name').value = "";
     document.getElementById('email').value = "";
     document.getElementById('age').value = "";
+    document.getElementById('hobby').value = "";
 }
 
 function addPersonToList(person) {
     const container = document.getElementById("people-list");
 
-    const personInfo = document.createElement("li");
-    personInfo.textContent = `Name: ${person.name}, Email: ${person.email}, Age: ${person.age}`;
+    const personInfo = document.createElement("div");
+    personInfo.innerHTML = `<p><strong>Name:</strong> ${person.name}</p><p><strong>Email:</strong> ${person.email}</p><p><strong>Age:</strong> ${person.age}</p><p><strong>Hobby:</strong> ${person.hobby}</p>`;
 
     container.appendChild(personInfo);
 }
